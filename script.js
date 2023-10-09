ef1148c29adad7c9f2ace6fb309fdee0cb9f28d3
Sashank <Sashank.pilli@gmail.com>
2023-10-09 21:21:52 +0530

    removed-animations

--- script.js
+++ script.js
@@ -11,44 +11,44 @@ card.addEventListener('mouseleave', () => {
 //User interaction animations using animate css
 // Select elements
 // Get section and animated elements
-const sections = [
-  document.querySelector('#about'),
-  document.querySelector('#projects'),
-  document.querySelector('#skills'),
-  document.querySelector('#contact'),
-];
-const animatedEls = document.querySelectorAll('[data-animate]');
+// const sections = [
+//   document.querySelector('#about'),
+//   document.querySelector('#projects'),
+//   document.querySelector('#skills'),
+//   document.querySelector('#contact'),
+// ];
+// const animatedEls = document.querySelectorAll('[data-animate]');
 
-// Callback for both section and elements
-const animateCallback = (entries, observer) => {
-  entries.forEach((entry) => {
-    const isSection = sections.includes(entry.target);
-    if (isSection) {
-      // Logic for section
-      if (entry.isIntersecting) {
-        entry.target.classList.remove('invisible');
-      }
-    } else {
-      // Logic for animated elements
-      if (entry.isIntersecting && window.innerWidth >= 768) {
-        entry.target.classList.remove('invisible');
-        entry.target.classList.add(entry.target.dataset.animate);
-      }
-    }
-  });
-};
+// // Callback for both section and elements
+// const animateCallback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     const isSection = sections.includes(entry.target);
+//     if (isSection) {
+//       // Logic for section
+//       if (entry.isIntersecting) {
+//         entry.target.classList.remove('invisible');
+//       }
+//     } else {
+//       // Logic for animated elements
+//       if (entry.isIntersecting && window.innerWidth >= 768) {
+//         entry.target.classList.remove('invisible');
+//         entry.target.classList.add(entry.target.dataset.animate);
+//       }
+//     }
+//   });
+// };
 
-// Create a single observer
-const observer = new IntersectionObserver(animateCallback, {
-  threshold: 1,
-});
+// // Create a single observer
+// const observer = new IntersectionObserver(animateCallback, {
+//   threshold: 0.5,
+// });
 
-// Observe sections
-sections.forEach((section) => {
-  observer.observe(section);
-});
+// // Observe sections
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
 
-// Observe animated elements
-animatedEls.forEach((el) => {
-  observer.observe(el);
-});
+// // Observe animated elements
+// animatedEls.forEach((el) => {
+//   observer.observe(el);
+// });
